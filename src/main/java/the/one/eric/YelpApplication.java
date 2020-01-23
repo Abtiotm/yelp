7089ceac51f7290f928290205fdff4438100fd72
obito-segu <925394401@qq.com>
2020-01-23 10:11:45 +0800

    init

---
+++ src/main/java/the/one/eric/YelpApplication.java
@@ -0,0 +1,13 @@
+package the.one.eric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpApplication.class, args);
+	}
+
+}
